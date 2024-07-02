2a6cd69a71c2c932632e0897f10bd4f33c192a4d
Amiyansu Patra <139028543+Amiyansu@users.noreply.github.com>
2024-07-03 01:32:11 +0530

    vercel2 commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
-import userRouter from "./routes/userRoute.js";
+import { connectDB } from "../config/db.js";
+import foodRouter from "../routes/foodRoute.js";
+import userRouter from "../routes/userRoute.js";
 import "dotenv/config";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
+import cartRouter from "../routes/cartRoute.js";
+import orderRouter from "../routes/orderRoute.js";
 
 //app config
 const app = express();
 const port = process.env.PORT || 8000;
 
-//middlewaree
+//middleware
 app.use(cors());
 app.use(express.json());
 
@@ -20,7 +20,7 @@ connectDB();
 
 // API endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static("uploads")); //access of uploaded images from uploads folder
+app.use("/images", express.static("../uploads")); // Adjusted path
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
